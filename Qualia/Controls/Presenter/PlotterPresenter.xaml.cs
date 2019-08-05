fabb79b4b17c32c5450e0953a3da579e63057df4
Echoviser <echoviser@gmail.com>
2019-08-05 14:44:03 +0700

    Init.

---
+++ Qualia/Controls/Presenter/PlotterPresenter.xaml.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Qualia.Controls
+{
+    /// <summary>
+    /// Interaction logic for PlotterPresenter.xaml
+    /// </summary>
+    public partial class PlotterPresenter : UserControl
+    {
+        public PlotterPresenter()
+        {
+            InitializeComponent();
+        }
+    }
+}
