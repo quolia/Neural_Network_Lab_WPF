5cb8bb89d9922f89089bd4a942e959bf9e5948f3
Echoviser <echoviser@gmail.com>
2019-08-10 16:39:29 +0700

    Keep window aspect ratio.

--- Qualia/MainWindow.xaml.cs
+++ Qualia/MainWindow.xaml.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
@@ -12,6 +13,7 @@ using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
+using System.Windows.Interop;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
@@ -37,9 +39,102 @@ namespace Qualia
             InitializeComponent();
          
             Loaded += Main_Load;
+            SourceInitialized += Window_SourceInitialized;
+        }
 
-            //SetStyle(ControlStyles.UserPaint | ControlStyles.OptimizedDoubleBuffer | ControlStyles.AllPaintingInWmPaint, true);
-            //SetStyle(ControlStyles.SupportsTransparentBackColor, false);
+        bool? _adjustingHeight = null;
+
+        internal enum SWP
+        {
+            NOMOVE = 0x0002
+        }
+        internal enum WM
+        {
+            WINDOWPOSCHANGING = 0x0046,
+            EXITSIZEMOVE = 0x0232,
+        }
+
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct WINDOWPOS
+        {
+            public IntPtr hwnd;
+            public IntPtr hwndInsertAfter;
+            public int x;
+            public int y;
+            public int cx;
+            public int cy;
+            public int flags;
+        }
+
+        [DllImport("user32.dll")]
+        [return: MarshalAs(UnmanagedType.Bool)]
+        internal static extern bool GetCursorPos(ref Win32Point pt);
+
+        [StructLayout(LayoutKind.Sequential)]
+        internal struct Win32Point
+        {
+            public Int32 X;
+            public Int32 Y;
+        };
+
+        public static Point GetMousePosition()
+        {
+            Win32Point w32Mouse = new Win32Point();
+            GetCursorPos(ref w32Mouse);
+            return new Point(w32Mouse.X, w32Mouse.Y);
+        }
+
+
+        private void Window_SourceInitialized(object sender, EventArgs ea)
+        {
+            HwndSource hwndSource = (HwndSource)HwndSource.FromVisual((Window)sender);
+            hwndSource.AddHook(DragHook);
+        }
+
+        private IntPtr DragHook(IntPtr hwnd, int msg, IntPtr wParam, IntPtr lParam, ref bool handled)
+        {
+            switch ((WM)msg)
+            {
+                case WM.WINDOWPOSCHANGING:
+                    {
+                        WINDOWPOS pos = (WINDOWPOS)Marshal.PtrToStructure(lParam, typeof(WINDOWPOS));
+
+                        if ((pos.flags & (int)SWP.NOMOVE) != 0)
+                            return IntPtr.Zero;
+
+                        Window wnd = (Window)HwndSource.FromHwnd(hwnd).RootVisual;
+                        if (wnd == null)
+                        {
+                            return IntPtr.Zero;
+                        }
+
+                        if (!_adjustingHeight.HasValue)
+                        {
+                            Point p = GetMousePosition();
+
+                            double diffWidth = Math.Min(Math.Abs(p.X - pos.x), Math.Abs(p.X - pos.x - pos.cx));
+                            double diffHeight = Math.Min(Math.Abs(p.Y - pos.y), Math.Abs(p.Y - pos.y - pos.cy));
+
+                            _adjustingHeight = diffHeight > diffWidth;
+                        }
+
+                        if (_adjustingHeight.Value)
+                            pos.cy = (int)(9 * pos.cx / 16);
+                        else
+                            pos.cx = (int)(16 * pos.cy / 9);
+
+                        Marshal.StructureToPtr(pos, lParam, true);
+                        handled = true;
+                    }
+                    break;
+
+                case WM.EXITSIZEMOVE:
+                    _adjustingHeight = null;
+                    break;
+            }
+
+            return IntPtr.Zero;
         }
 
         private void Main_Load(object sender, EventArgs e)
