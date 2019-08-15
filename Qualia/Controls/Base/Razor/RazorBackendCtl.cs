d873498c0b223df3f9149c8b39d5c9b137e6f346
Echoviser <echoviser@gmail.com>
2019-08-15 21:06:07 +0700

    Razor added.

---
+++ Qualia/Controls/Base/Razor/RazorBackendCtl.cs
@@ -0,0 +1,23 @@
+﻿//   (c) Mokrov Ivan
+// special for habrahabr.ru
+// under MIT license
+
+using System.Windows.Forms;
+
+namespace Qualia.Controls
+{
+    public partial class RazorBackendCtl : UserControl
+    {
+        public RazorBackendCtl()
+        {
+            InitializeComponent();
+
+            SetStyle(ControlStyles.DoubleBuffer, false);
+            SetStyle(ControlStyles.UserPaint, true);
+            SetStyle(ControlStyles.AllPaintingInWmPaint, true);
+            SetStyle(ControlStyles.Opaque, true);
+
+            this.MinimumSize = new System.Drawing.Size(1, 1);
+        }
+    }
+}
